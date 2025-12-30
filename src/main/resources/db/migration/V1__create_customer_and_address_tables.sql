1b80a43667a78ece7830f96eec0ac841f59d2caa
Edson Cândido <edd.candido@gmail.com>
2025-12-30 20:29:00 -0300

    feat: add customer and address tables with validation messages and API specification

---
+++ src/main/resources/db/migration/V1__create_customer_and_address_tables.sql
@@ -0,0 +1,40 @@
+-- Sequence para tabela de Clientes
+CREATE SEQUENCE customer_id_seq START WITH 1 INCREMENT BY 50;
+
+-- Tabela de Clientes
+CREATE TABLE CUSTOMER (
+    id         BIGINT  PRIMARY KEY DEFAULT nextval('customer_id_seq'),
+    name       VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    cpf        VARCHAR(11)  NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    phone      VARCHAR(20)  NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uk_customer_cpf UNIQUE (cpf),
+    CONSTRAINT uk_customer_email UNIQUE (email)
+);
+
+-- Sequence para tabela de Endereços
+CREATE SEQUENCE address_id_seq START WITH 1 INCREMENT BY 50;
+
+-- Tabela de Endereços
+CREATE TABLE ADDRESS (
+    id           BIGINT    PRIMARY KEY DEFAULT nextval('address_id_seq'),
+    street       VARCHAR(255) NOT NULL,
+    number       INTEGER      NOT NULL,
+    complement   VARCHAR(255),
+    neighborhood VARCHAR(100) NOT NULL,
+    city         VARCHAR(100) NOT NULL,
+    state        VARCHAR(2)   NOT NULL,
+    zip_code     VARCHAR(20)  NOT NULL,
+    country      VARCHAR(100) NOT NULL,
+    type         VARCHAR(100)  NOT NULL,
+    customer_id  BIGINT       NOT NULL,
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_address_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES CUSTOMER (id)
+        ON DELETE CASCADE
+);
