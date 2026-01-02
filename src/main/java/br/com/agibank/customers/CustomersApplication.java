f63fe19d50fd22bda09e82728c8cbb7e94374e3f
Edson Cândido <edd.candido@gmail.com>
2026-01-02 19:55:24 -0300

    refactor: update DateTimeProvider configuration to use constant for provider reference

--- src/main/java/br/com/agibank/customers/CustomersApplication.java
+++ src/main/java/br/com/agibank/customers/CustomersApplication.java
@@ -2,10 +2,14 @@ package br.com.agibank.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
 
+import static br.com.agibank.customers.infrastructure.configs.DateTimeProviderConfig.OFFSET_DATE_TIME_PROVIDER;
+
 @SpringBootApplication
+@EnableJpaAuditing(dateTimeProviderRef = OFFSET_DATE_TIME_PROVIDER)
 public class CustomersApplication {
 
     public static void main(String[] args) {
--- src/main/java/br/com/agibank/customers/infrastructure/configs/DateTimeProviderConfig.java
+++ src/main/java/br/com/agibank/customers/infrastructure/configs/DateTimeProviderConfig.java
@@ -3,16 +3,16 @@ package br.com.agibank.customers.infrastructure.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.OffsetDateTime;
 import java.util.Optional;
 
 @Configuration
-@EnableJpaAuditing(dateTimeProviderRef = "offsetDateTimeProvider")
 public class DateTimeProviderConfig {
 
-    @Bean
+    public static final String OFFSET_DATE_TIME_PROVIDER = "offsetDateTimeProvider";
+
+    @Bean(OFFSET_DATE_TIME_PROVIDER)
     public DateTimeProvider offsetDateTimeProvider() {
         return () -> Optional.of(OffsetDateTime.now());
     }
