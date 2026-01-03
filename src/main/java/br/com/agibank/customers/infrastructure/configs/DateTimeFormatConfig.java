f587434c92bf9c5883743efe0d75062fb31f3ca0
Edson Cândido <edd.candido@gmail.com>
2026-01-03 13:34:53 -0300

    refactor: rename JacksonDateTimeFormatConfig to DateTimeFormatConfig and update date-time formatting to use configurable business timezone

--- src/main/java/br/com/agibank/customers/infrastructure/configs/DateTimeFormatConfig.java
+++ src/main/java/br/com/agibank/customers/infrastructure/configs/DateTimeFormatConfig.java
@@ -3,6 +3,7 @@ package br.com.agibank.customers.infrastructure.configs; // Ou o pacote de confi
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,19 +16,27 @@ import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 import static com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer.INSTANCE;
 
 @Configuration
-public class JacksonDateTimeFormatConfig {
+public class DateTimeFormatConfig {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER =
-            DateTimeFormatter.ofPattern("dd-MM-yyyy'T'HH:mm:ssXXX").withZone(ZoneId.of("America/Sao_Paulo"));
+    private static final String DATE_TIME_FORMAT = "dd-MM-yyyy'T'HH:mm:ssXXX";
+
+    @Value("${app.business-timezone}")
+    private String businessTimezone;
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        final var formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT).withZone(businessZoneId());
         return builder -> {
-            final JavaTimeModule javaTimeModule = new JavaTimeModule();
+            final var javaTimeModule = new JavaTimeModule();
             javaTimeModule.addSerializer(OffsetDateTime.class,
-                    new OffsetDateTimeSerializer(INSTANCE, false, DATE_TIME_FORMATTER, STRING));
+                    new OffsetDateTimeSerializer(INSTANCE, false, formatter, STRING));
             builder.modules(javaTimeModule);
             builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         };
+    }
+
+    @Bean
+    public ZoneId businessZoneId() {
+        return ZoneId.of(businessTimezone);
     }
 }
